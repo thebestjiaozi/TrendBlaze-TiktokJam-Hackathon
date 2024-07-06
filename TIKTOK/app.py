5e676351c55efd862e50395d212532a7d9bf8b0c
Li Siqi <163830493+thebestjiaozi@users.noreply.github.com>
2024-07-06 21:20:25 +0800

    Update app.py

--- TIKTOK/app.py
+++ TIKTOK/app.py
@@ -16,8 +16,8 @@ from process import analytics
 import os
 
 def main():
-    os.system('playwright install')
-    os.system('playwright install-deps')
+    os.system('python -m playwright install')
+
     # Default settings
     if 'followers' not in st.session_state:
         st.session_state.followers = 0
